e8a107d5dbc2749dd11e48c94c7c95a113a42e15
pafilenko <pafilenko@avito.ru>
2025-02-18 21:46:45 +0300

    Implement create user step

--- src/main/java/com/example/teamcity/api/spec/Specifications.java
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -2,7 +2,6 @@ package com.example.teamcity.api.spec;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
-import io.restassured.authentication.AuthenticationScheme;
 import io.restassured.authentication.BasicAuthScheme;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -15,31 +14,34 @@ import java.util.List;
 public class Specifications {
     private static Specifications spec;
 
-    private Specifications() {};
-
-    public static Specifications getSpec() {
-        if (spec == null) {
-            spec = new Specifications();
-        }
-        return spec;
-    }
-
-    private RequestSpecBuilder reqBuilder() {
+    private static RequestSpecBuilder reqBuilder() {
         RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
-        reqBuilder.setBaseUri("http://" + Config.getProperty("host")).build();
+        reqBuilder.setBaseUri("http://%s".formatted(Config.getProperty("host"))).build();
         reqBuilder.setContentType(ContentType.JSON);
         reqBuilder.setAccept(ContentType.JSON);
         reqBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
         return reqBuilder;
     }
 
-    public RequestSpecification unauthSpec() {
+    public static RequestSpecification superUserAuthSpec() {
+        BasicAuthScheme basicAuthScheme = new BasicAuthScheme();
+        basicAuthScheme.setUserName("");
+        basicAuthScheme.setPassword(Config.getProperty("superUserToken"));
+        return reqBuilder()
+                .setAuth(basicAuthScheme)
+                .build();
+//        return reqBuilder()
+//                .setBaseUri("http://%s:%s@%s/httpAuth".formatted("", Config.getProperty("superUserToken"), Config.getProperty("host")))
+//                .build();
+    }
+
+    public static RequestSpecification unauthSpec() {
         return reqBuilder().build();
     }
 
-    public RequestSpecification authSpec(User user) {
+    public static RequestSpecification authSpec(User user) {
         BasicAuthScheme basicAuthScheme = new BasicAuthScheme();
-        basicAuthScheme.setUserName(user.getUser());
+        basicAuthScheme.setUserName(user.getUsername());
         basicAuthScheme.setPassword(user.getPassword());
         return reqBuilder()
                 .setAuth(basicAuthScheme)
--- src/test/java/com/example/teamcity/api/DummyTest.java
+++ src/test/java/com/example/teamcity/api/DummyTest.java
@@ -10,7 +10,7 @@ public class DummyTest extends BaseApiTest {
     public void userShouldBeAbleGetAllProjects() {
         RestAssured
                 .given()
-                .spec(Specifications.getSpec().authSpec(User.builder().user("admin").password("admin").build()))
+                .spec(Specifications.authSpec(User.builder().username("admin").password("admin").build()))
                 .get("/app/rest/projects");
         }
 }
