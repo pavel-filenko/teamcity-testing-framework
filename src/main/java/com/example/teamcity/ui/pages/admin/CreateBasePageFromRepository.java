638dfae30f1da819c0581313400e1b6d17d97607
pafilenko <pafilenko@avito.ru>
2025-04-11 17:00:13 +0300

    fix import

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateBasePageFromRepository.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateBasePageFromRepository.java
@@ -3,7 +3,6 @@ package com.example.teamcity.ui.pages.admin;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
-import com.example.teamcity.ui.pages.BasePage;
 
 import static com.codeborne.selenide.Selenide.$;
 
