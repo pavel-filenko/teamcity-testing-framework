5b417fe4b1ce6eaf9af34871caab7843e6e79ac6
pafilenko <pafilenko@avito.ru>
2025-04-11 16:59:48 +0300

    fix import

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateProjectPageFromRepository.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateProjectPageFromRepository.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.page;
 
 public class CreateProjectPageFromRepository extends CreateBasePageFromRepository {
 
